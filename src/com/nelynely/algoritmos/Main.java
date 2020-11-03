7f454c7612b96927496ba04aaab4d0e859c3f353
nely de faria <nelly.nf54@gmail.com>
2020-11-03 16:40:28 -0300

    Implementação do algoritmo de busca linear

--- src/com/nelynely/algoritmos/Main.java
+++ src/com/nelynely/algoritmos/Main.java
@@ -8,17 +8,37 @@ public class Main {
 		int[] numeros = new int[6];
 		
 		Scanner scan = new Scanner(System.in);
-		//Escrita dos dados
 		for (int i = 0; i < numeros.length; i++) {
 			System.out.print("Informe um número: ");
 			int numero = scan.nextInt();
 			numeros[i] = numero;
 		}
-		//Leitura dos dados
+		// **Busca liner
+		System.out.println("** BUSCA LINEAR **");
+		System.out.println("Digite o número a ser pesquisado: ");
+		int alvo = scan.nextInt();
+		int posicaoResultado = -1;
 		for (int i = 0; i < numeros.length; i++) {
-			System.out.println(numeros[i]);
+			if (numeros[i] == alvo) {
+				posicaoResultado = i;
+				break;
+			}
 		}
+		if (posicaoResultado < 0) {
+			System.out.println("Elemento não foi encontrado: ");
+		} else {
+			System.out.println(String.format("O número %d foi encontrado na posição %d", alvo, posicaoResultado));
+		}
+		// ** FIM BUSCA LINEAR **
+		System.out.println("Vetor: ");
+		imprimirArray(numeros);
 		scan.close();
+	}
+	
+	private static void imprimirArray(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
 	}
 
 }
